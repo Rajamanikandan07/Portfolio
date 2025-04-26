b02e68075206b0a5e425fefedde8346586553ac8
unknown <rajamanikandankit@gmail.com>
2025-04-26 10:57:04 +0530

    resume update

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,6 +1,7 @@
 import './home.css'
 import mani from '../heroimg/manikandan.png';
 import { useTypewriter } from 'react-simple-typewriter'
+import resume from '../../assets/Rajamanikandan resume.pdf';
 
 export default function Home() {
 
@@ -21,7 +22,7 @@ export default function Home() {
                     <br />
                     <p>Expertise in building dynamic web applications. Skilled in front-end technologies like Html, Css, Bootstrap, Javascript, React, and back-end frameworks such as Python and Django, I create seamless, user-friendly experiences. I have experience with databases like PostgreSQL, and I focus on writing clean, scalable code. Passionate about learning and staying up-to-date with new technologies, I strive to deliver high-quality, performant web solutions.</p>
                     <br />
-                    <a href="" target='_blank'><button className='resume-btn'>Resume</button></a>
+                    <a href={resume} download='Rajamanikandan.pdf' target='_blank'><button className='resume-btn'>Resume</button></a>
                 </div>
                 <div className="user-img-box">
                     <div className="img-box">
